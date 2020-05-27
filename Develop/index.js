f51fe5d1555dae1f88d4ccc7351c9950161af372
ICVRXS <calebhay@rocketmail.com>
2020-05-27 11:30:36 -0700

    first question prompt functioning

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,22 @@
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
+const questions = [
+    {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "projectName"
+    }
 ];
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    inquirer.prompt(questions).then((response) => {
+        writeToFile("README.md", response);
+    });
 }
 
 init();
