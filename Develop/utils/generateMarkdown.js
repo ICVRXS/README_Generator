390c405d38b469b874a2bae26fac731b97dc4a1a
ICVRXS <calebhay@rocketmail.com>
2020-05-29 19:41:02 -0700

    progam now creates generic title if user does not designate one

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,34 +1,48 @@
 function generateMarkdown(data) {
-  return `# ${data.projectName}
-  ${data.projectDesc}
+  let fileContent = "";
+  if (!data.projectName){
+    fileContent += "# My Project";
+    return fileContent;
+    // return "You must enter a name for your project.";
+  }else{
+    fileContent += generateTitle(data.projectName);
+    return fileContent;
+  }
 
-# Table of Contents
-  * Installation
-  * Usage
-  * Licenses
-  * Contributing
-  * Testing
-  * FAQ
-
-# Installation
-  ${data.projectInstall}
-
-# Usage
-  ${data.projectUsage}
-
-# Licensing
-  ${data.projectLicense}
-
-# Contributors
-  ${data.projectContribute}
-
-# Testing
-  ${data.projectTest}
-
-# FAQ
-  ${data.projectFaq}
-
-`;
+  function generateTitle(data){
+    return `# ${data}`
+  }
 }
+
+//   ${data.projectDesc}
+
+// # Table of Contents
+//   * Installation
+//   * Usage
+//   * Licenses
+//   * Contributing
+//   * Testing
+//   * FAQ
+
+// # Installation
+//   ${data.projectInstall}
+
+// # Usage
+//   ${data.projectUsage}
+
+// # Licensing
+//   ${data.projectLicense}
+
+// # Contributors
+//   ${data.projectContribute}
+
+// # Testing
+//   ${data.projectTest}
+
+// # FAQ
+//   ${data.projectFaq}
+
+// `;
+// }
 
 module.exports = generateMarkdown;
